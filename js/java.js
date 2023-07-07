fa03665bb601a1371e747b14fc7bae584ae304e4
Clifford William <ccliffordwilliam@gmail.com>
2023-07-07 12:29:45 +0800

    initial commit

---
+++ js/java.js
@@ -0,0 +1,66 @@
+// Toggle menu visibility
+function toggleMenu() {
+    var navMenu = document.querySelector('.nav-menu');
+    var svgBurger = document.querySelector('.svg-burger');
+    var svgCross = document.querySelector('.svg-cross');
+    
+    navMenu.classList.toggle('active');
+    svgBurger.classList.toggle('hide');
+    svgCross.classList.toggle('hide');
+  }
+
+// Get all the dropdown menu items and their corresponding submenus
+const dropdownItems = document.querySelectorAll('.drop');
+const submenus = document.querySelectorAll('.drop-menu');
+
+// Add event listeners for mouseenter and mouseleave events to each dropdown menu item
+dropdownItems.forEach((dropdownItem, index) => {
+  const submenu = submenus[index];
+
+  dropdownItem.addEventListener('mouseenter', () => {
+    showSubMenu(submenu);
+  });
+
+  dropdownItem.addEventListener('mouseleave', () => {
+    hideSubMenu(submenu);
+  });
+});
+
+// Function to show the submenu
+function showSubMenu(submenu) {
+  submenu.classList.toggle('show');
+  // submenu.style.display = 'block';
+}
+
+// Function to hide the submenu
+function hideSubMenu(submenu) {
+  submenu.classList.toggle('show');
+  // submenu.style.display = 'none';
+}
+
+// Fades in and out fading images
+const fadingImages = document.querySelectorAll('.fading-img');
+const fadeDuration = 6000; // Duration in milliseconds
+
+let currentIndex = 0;
+
+function fadeOut() {
+  fadingImages[currentIndex].style.opacity = '0';
+}
+
+function fadeIn() {
+  fadingImages[currentIndex].style.opacity = '1';
+}
+
+function fadeImages() {
+  fadeOut();
+
+  currentIndex = (currentIndex + 1) % fadingImages.length;
+
+  fadeIn();
+}
+
+setInterval(fadeImages, fadeDuration);
+
+
+  
